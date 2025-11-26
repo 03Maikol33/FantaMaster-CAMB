1c906900be424845d236a73e7b7ed8e942383cc0
Bassma4 <boubassma9@gmail.com>
2025-11-26 19:02:40 +0100

    Sistemato il gitignore

--- Java_Project/fantamaster/src/main/java/it/camb/fantamaster/util/ConnectionFactory.java
+++ Java_Project/fantamaster/src/main/java/it/camb/fantamaster/util/ConnectionFactory.java
@@ -1,3 +1,17 @@
+/*
+ * Questo file contiene credenziali. NON committare nel repository.
+ *
+ * Per ignorare con git, aggiungi nella radice del repo una riga in .gitignore:
+ * src/main/java/it/camb/fantamaster/util/ConnectionFactory.java
+ *
+ * Se il file è già tracciato, rimuovilo dall'index e committa:
+ * git rm --cached src/main/java/it/camb/fantamaster/util/ConnectionFactory.java
+ * git commit -m "Remove sensitive ConnectionFactory from index"
+ *
+ * Consigliato: sposta le credenziali in un file di configurazione non tracciato
+ * o usa variabili d'ambiente/properties esterni.
+ */
+
 package it.camb.fantamaster.util;
 
 import java.sql.Connection;
@@ -5,10 +19,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    // Parametri di configurazione del DB
-    private static final String URL = "jdbc:mysql://localhost:3306/fantamaster?useSSL=false&serverTimezone=UTC";
+    // Parametri di configurazione del 
+    private static final String URL = "jdbc:mysql://localhost:3306/fantamaster?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "pass123";
 
     // Metodo statico per ottenere una connessione
     public static Connection getConnection() throws SQLException {
