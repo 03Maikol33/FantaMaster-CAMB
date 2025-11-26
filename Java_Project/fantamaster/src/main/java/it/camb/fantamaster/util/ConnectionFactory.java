c062cf3f6193bcff22ebf79a59d88ff08c0245cb
03Maikol33 <117536575+03Maikol33@users.noreply.github.com>
2025-11-26 19:05:03 +0100

    Ripristino di connection factory

--- Java_Project/fantamaster/src/main/java/it/camb/fantamaster/util/ConnectionFactory.java
+++ Java_Project/fantamaster/src/main/java/it/camb/fantamaster/util/ConnectionFactory.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+    //legge i paramentri di connessione da un file di configurazione chiamato dbCredentials.properties che sta su "C:"
+    private static final String CONFIG_FILE_PATH = "C:/dbCredentials.properties";
+    
     // Parametri di configurazione del DB
     private static final String URL = "jdbc:mysql://localhost:3306/fantamaster?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
